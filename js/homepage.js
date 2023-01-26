123f95079042dd9446882ebf265e03e0d8c5679b
jacktembo <jack@jacktembo.com>
2023-01-26 11:17:31 +0200

    ...

--- js/homepage.js
+++ js/homepage.js
@@ -1,27 +1,35 @@
-"use strict";
+let token = "b1136fb60f5b0484cac2827b8642b55b6f2e517a";
+let response;
+let companyList = document.querySelector(".companies-cards");
 
-var token = 'b1136fb60f5b0484cac2827b8642b55b6f2e517a';
-var response;
-var companyList = document.querySelector('.companies-cards');
-fetch('https://buses.pridezm.com/api/bus-companies', {
-  method: 'GET',
+fetch("https://buses.pridezm.com/api/bus-companies", {
+  method: "GET",
   headers: {
-    'Authorization': "Token ".concat(token)
-  }
-}).then(function (res) {
-  return res.json();
-}).then(function (data) {
-  response = data;
-  console.log(response);
-  response.forEach(function (element) {
-    var item = document.createElement("div");
-    item.id = element.id;
-    item.innerHTML = "\n        <a href=\"\" class=\"company-link\">\n        <div class=\"company-card\" style=\"flex: 1 1 50%;\">\n            <img src=\"".concat(element.image, "\" alt=\"\" class=\"top-img\">\n            <div class=\"company-name\"> ").concat(element.company_name, " </div>\n        </div>\n      </div>\n        </a>\n        ");
-    companyList.appendChild(item);
-  });
-})["catch"](function (error) {
-  return console.error(error);
-});
-var footer = document.querySelector('footer');
+    Authorization: `Token ${token}`,
+  },
+})
+  .then((res) => res.json())
+
+  .then((data) => {
+    response = data;
+    console.log(response);
+    response.forEach((element) => {
+      let item = document.createElement("div");
+      item.id = element.id;
+      item.innerHTML = `
+        <a href="" class="company-link">
+        <div class="company-card" style="flex: 1 1 50%;">
+            <img src="${element.image}" alt="" class="top-img">
+            <div class="company-name"> ${element.company_name} </div>
+        </div>
+      </div>
+        </a>
+        `;
+      companyList.appendChild(item);
+    });
+  })
+  .catch((error) => console.error(error));
+
+let footer = document.querySelector("footer");
 companyList.after(footer);
-footer.style.display = 'block';
+footer.style.display = "block";
