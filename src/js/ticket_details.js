71368f0fa6a8e9c859790543d246692d62b1caa8
jacktembo <jack@jacktembo.com>
2023-01-31 15:05:12 +0200

    route filter logic is done

--- src/js/ticket_details.js
+++ src/js/ticket_details.js
@@ -1,11 +1,73 @@
-let company_name = sessionStorage.getItem('company_name')
-let company_id = sessionStorage.getItem('company_id')
-let company_image_url = sessionStorage.getItem('company_image_url')
-let banner_img = document.querySelector('.banner-img')
-banner_img.src = company_image_url
-document.querySelector('h1 span').innerHTML = company_name
-let journeyForm = document.querySelector('.journey')
-let startingPlace = journeyForm.querySelector('#starting-place')
-let destination = document.querySelector('#destination')
-let departure_date = document.querySelector('#departure-date')
-let FindBusButton = document.querySelector('.find-bus')
+let company_name = sessionStorage.getItem("company_name");
+let company_id = sessionStorage.getItem("company_id");
+let company_image_url = sessionStorage.getItem("company_image_url");
+let banner_img = document.querySelector(".banner-img");
+banner_img.src = company_image_url;
+let token = "b1136fb60f5b0484cac2827b8642b55b6f2e517a";
+document.querySelector("h1 span").innerHTML = company_name;
+let journeyForm = document.querySelector(".journey");
+let startingPlace = journeyForm.querySelector("#starting-place");
+let destination = document.querySelector("#destination");
+let departure_date = document.querySelector("#departure-date");
+let FindBusButton = document.querySelector(".find-bus");
+let towns;
+let townSelect = document.getElementById("starting-place");
+let destinationSelect = document.getElementById("destination");
+fetch(`https://buses.pridezm.com/api/routes?bus-company=${company_id}`, {
+  method: "GET",
+  headers: {
+    Authorization: `Token ${token}`,
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    let arr = data.filter((obj, index) => {
+      return (
+        data
+          .map((mapObj) => mapObj["starting_place"])
+          .indexOf(obj["starting_place"]) === index
+      );
+    });
+    arr.forEach((town) => {
+      const option = document.createElement("option");
+      option.value = town.starting_place;
+      option.text = town.starting_place;
+      townSelect.add(option);
+    });
+
+    //   .catch((err) => console.log(err))
+  });
+
+townSelect.addEventListener("change", function () {
+  while (destinationSelect.options.length) {
+    destinationSelect.remove(0);
+  }
+  console.log(townSelect.value);
+  let selectedValue = townSelect.value;
+  fetch(
+    `https://buses.pridezm.com/api/routes?bus-company=${company_id}&starting_place=${selectedValue}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let arr = data.filter((obj, index) => {
+        return (
+          data
+            .map((mapObj) => mapObj["destination"])
+            .indexOf(obj["destination"]) === index
+        );
+      });
+      console.log(arr);
+      arr.forEach((town) => {
+        let option = document.createElement("option");
+        option.value = town.destination;
+        option.text = town.destination;
+        destinationSelect.add(option);
+      });
+    });
+});
