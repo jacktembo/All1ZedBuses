16895aa96168e6a6f8c74dce8f4a7e0db72dc377
jacktembo <jack@jacktembo.com>
2023-02-07 12:42:58 +0200

    payment waiting page done

--- js/confirm_payment.js
+++ js/confirm_payment.js
@@ -5,6 +5,7 @@ let seatNumber = sessionStorage.getItem('seatNumber')
 let insuranceType = sessionStorage.getItem('insuranceType')
 let routeId = sessionStorage.getItem('routeId')
 let departureDate = sessionStorage.getItem('departureDate')
+let token = "b1136fb60f5b0484cac2827b8642b55b6f2e517a";
 
 async function postData(url = '', data = {}) {
     const response = await fetch(url, {
@@ -37,6 +38,45 @@ async function postData(url = '', data = {}) {
     }
   }
   
-  // makePayment();
+  makePayment();
 
+  setInterval(function() {
+    if (sessionStorage.getItem('paymentComplete') !== null) {
+      // Hide loader if the value is available
+      document.querySelector('.container').style.display = 'none';
+    }
+  }, 100);
+
+
+  const interval = 15000; // 15 seconds
+  const timeout = 120000; // 2 minutes
+  
+  async function makeRequest() {
+    try {
+      const response = await fetch('https://buses.pridezm.com/api/pay-query', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`,
+        },
+        body: JSON.stringify({ pending_id: sessionStorage.getItem('pendingId') }),
+      });
+      const data = await response.json();
+      console.log(data);
+  
+      if (data.status === 'successful') {
+        sessionStorage.setItem('paymentComplete', true)
+        console.log('Payment successful');
+        clearInterval(intervalId);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  const intervalId = setInterval(makeRequest, interval);
+  setTimeout(() => clearInterval(intervalId), timeout);
+  setTimeout(() => {
+    document.querySelector('.container').style.display = 'none';
+  }, timeout)
   
