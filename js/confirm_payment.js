d5d91925da9cba9f9626f0743fa49e6af43070bd
jacktembo <jack@jacktembo.com>
2023-02-08 14:37:49 +0200

    ...

--- js/confirm_payment.js
+++ js/confirm_payment.js
@@ -41,13 +41,13 @@ async function postData(url = '', data = {}) {
   
   makePayment();
 
-  setInterval(function() {
-    if (sessionStorage.getItem('paymentComplete') !== null) {
-      // Hide loader if the value is available
-      document.querySelector('.container').style.display = 'none';
-      document.location = '../payment_status.html'
-    }
-  }, interval);
+  // setInterval(function() {
+  //   if (sessionStorage.getItem('paymentComplete') !== null) {
+  //     // Hide loader if the value is available
+  //     document.querySelector('.container').style.display = 'none';
+  //     document.location = '../payment_status.html'
+  //   }
+  // }, interval);
 
 
  
